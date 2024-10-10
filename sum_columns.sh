8422910d4b18b2cb32ee07b6fc1d087fadd476f1
Jason Toy <38897557+jtoy7@users.noreply.github.com>
2024-10-10 16:18:45 -0700

    Create sum_columns.sh

---
+++ sum_columns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if the file is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+file=$1
+
+# Use awk to process the file
+awk '
+NR > 1 {
+    for (i = 2; i <= NF; i++) {
+        sum[i] += $i
+    }
+}
+END {
+    for (i = 2; i <= NF; i++) {
+        printf "Sum of column %d: %s\n", i, sum[i]
+    }
+}
+' "$file"
